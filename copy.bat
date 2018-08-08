d6a8219452be414844cf884c44597546d9d435ad
Abhinaw kumar <abhinaw27392@gmail.com>
2018-08-08 15:16:52 +0530

    changes to copy.bat

--- copy.bat
+++ copy.bat
@@ -1,1 +1,1 @@
-xcopy  E:\AHI_TimeSheet\ui_new\build  E:\AHI_TimeSheet\backend\src\main\resources\public /E
+xcopy  E:\AHI_TimeSheet\ahi_timesheet_frontend\build  E:\AHI_TimeSheet\backend\src\main\resources\public /E
